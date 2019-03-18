2666e0424dabbd49ac75318732de33f2b3f63aa1
lengocchau <47291548+lengocchau@users.noreply.github.com>
2019-03-18 12:09:30 +0700

    Update ex1.php
    Using a null array, then use for loop to add value in, then use 'implode' to convert array to string and print it out with 'echo' outside the loop

--- ex1.php
+++ ex1.php
@@ -36,6 +36,48 @@ for ($x=1; $x <= 100 ; $x++) {
 		}
 	}	
 	
+$a = [];
+for ($i=1; $i <= 100 ; $i++) { 
+	$a[] = $i ;
+}
+echo implode($a, ' ') ;
+
+echo "<br>";
+echo "<br>";
+
+
+
+$number2 = range(0, 100);
+echo "<pre>" ;
+print_r($number2);
+echo "</pre>" ;
+
+echo "<br>";
+echo "<br>";
+
+$b = [];
+for ($i=0; $i <= 100 ; $i++) { 
+	if ($i % 2 <> 0) {
+		$b[] = $i ;
+	}
+}
+echo implode($b, ' ');
+
+echo "<br>";
+echo "<br>";
+
+
+$c = [];
+for ($i=0; $i <= 100 ; $i++) { 
+		if ($i % 3 == 0) {
+			$c[] = '('.$i.')' ; 
+		}
+			else {
+				$c[] = $i ;
+			}
+		}
+echo implode($c, ' ')	
+	
 ?>
 
 
